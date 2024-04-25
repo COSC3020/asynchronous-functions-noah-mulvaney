dea58cc005835d9df3b89564b074aa16b4cfce55
Noah Mulvaney <127539141+noah-mulvaney@users.noreply.github.com>
2024-04-25 09:34:13 -0600

    Implement asyncMin()

--- code.js
+++ code.js
@@ -1,11 +1,10 @@
 // COSC3020 Async Functions Exercise
 // Noah Mulvaney
-// 10 Apr 2024
-
-// Adapted from slides 21, 22 of parallelism lecture slides
+// 25 Apr 2024
 
 const {StaticPool} = require("node-worker-threads-pool");
 
+// Adapted from slides 21, 22 of parallelism lecture slides
 function nMatches(arr, key, done) {
   const nThreads = Math.floor(Math.sqrt(arr.length));
 
@@ -40,3 +39,34 @@ function nMatches(arr, key, done) {
     })();
   }
 }
+
+// asynchronously find the minimum value in an array 
+function asyncMin(arr, cb) {
+  const nThreads = Math.floor(Math.sqrt(arr.length)); // number of threads = sqrt{n}
+  let min = Infinity; // global variable to store minimum value found
+
+  // setup worker threads
+  const workers = new StaticPool({
+    size: nThreads,
+    task: function(subArr) {
+      for (let i = 0; i < subArr.length; ++i)
+        if (subArr[i] < this.workerData) this.workerData = subArr[i];
+    },
+    workerData: min // shared data, updated by all threads
+  });
+
+  const subSize = arr.length / nThreads; // size of sub arrays passed to each thread
+  let nFinished = 0; // number of threads finished
+
+  // assign work to threads
+  for (let i = 0; i < nThreads; ++i) {
+    (async () => {
+      // start thread working on sub array
+      await workerPool.exec(arr.slice(i * subSize, (i + 1) * subsize));
+      ++nFinished;
+
+      if (nFinished == nThreads) { // all threads have finished
+        cp(workers.workerData); // call the callback function with shared data
+        workers.destory(); // delete all the worker threads
+      }
+  }
